c9b6e633828dabb21f8d88588e05718440269ff9
Erik McLaughlin <erikm6872@gmail.com>
2015-11-16 17:42:11 -0700

    Comments
    Added comments to the MySQL script.

--- police_mysql.py
+++ police_mysql.py
@@ -10,11 +10,16 @@ import sys
 import mysql.connector
 
 def main():
+    
+    #Make sure the files are encoded in UTF-8
     reload(sys)
     sys.setdefaultencoding('utf8')
+    
     now = datetime.datetime.now()
     
-    tableName = "reports"
+    tableName = "reports"   #MySQL table to insert into
+    
+    #MySQL settings
     mysqlconfig = {
                     'user': '',
                     'password': '',
@@ -25,34 +30,31 @@ def main():
     cnx = mysql.connector.connect(**mysqlconfig)
     cursor = cnx.cursor()
     
+    #Retrieve lists of URLs and output file names
     urls = getURLs()
     fnames = getFNames()
-    successfulRows = 0
-    skippedRows = 0
     
+    #Array to hold generated INSERT statements
     sql_cmds = []
     
-    
-    
     for i in range(len(urls)):
-        tree = getData(urls[i])
-        sql_cmds.append(parseFile(tree, fnames[i], cursor, tableName))
-    print "Truncating table '" + tableName + "'..."
+        tree = getData(urls[i])                                             #Retrieve entire HTML text from page
+        sql_cmds.append(parseFile(tree, fnames[i], cursor, tableName))      #Send HTML to parseFile() method and add output to array
     
     print "\n\nInserting values into table '" + tableName + "'..."
     for i in sql_cmds:
         for j in i:
             #print repr(j)
-            cursor.execute(j)
-    cnx.commit()
-    cursor.close()
+            cursor.execute(j)   #Execute MySQL statements
+    cnx.commit()    #Commit changes to database
+    cursor.close()  
     cnx.close()
     print "\nDone."
     print "Processed %d pages from 7/2009 to %s/%d" % (len(urls), now.month, now.year)
-
-def insertMySQL(cursor, statement):
-    cursor.execute(statement)
     
+    
+#   Generates and returns a list of pages to scrape
+#   Ranges from June 2009 to the current month and year
 def getURLs():
     now = datetime.datetime.now()
     curYear = now.year
@@ -72,7 +74,9 @@ def getURLs():
     "December"
     ]
     curMonth = months[now.month - 1]
-    start_urls = [
+    
+    #Begin with array of pages from 2009
+    urls = [
                 "http://www.montana.edu/police/policelog/2009/June2009.html",
                 "http://www.montana.edu/police/policelog/2009/July2009.html",
                 "http://www.montana.edu/police/policelog/2009/August2009.html",
@@ -81,18 +85,23 @@ def getURLs():
                 "http://www.montana.edu/police/policelog/2009/November2009.html",
                 "http://www.montana.edu/police/policelog/2009/December2009.html"
                 ]
+    #Add pages to array from 2010 to the last complete year (2014 as of writing)
     for yr in range(2010, curYear):
         for mo in months:
             url = "http://www.montana.edu/police/policelog/%d/%s%d.html" % (yr, mo, yr)
-            start_urls.append(url)
+            urls.append(url)
+    #Add pages to array from current year
     for m in range(0, now.month):
         yr = curYear
         mo = months[m]
-        url = url = "http://www.montana.edu/police/policelog/%s/%s%s.html" % (yr, mo, yr)
-        start_urls.append(url)
+        url = "http://www.montana.edu/police/policelog/%s/%s%s.html" % (yr, mo, yr)
+        urls.append(url)
         
-    return start_urls
+    return urls
 
+    
+#   Same as getURLs() but without the URL formatting. 
+#   These two methods could probably be condensed into one at some point
 def getFNames():
     now = datetime.datetime.now()
     curYear = now.year
@@ -131,55 +140,73 @@ def getFNames():
         start_fnames.append("%d%s.txt" % (yr, mo))
         
     return start_fnames
-def getFile(fileName):
-    with open (fileName, "r") as myfile:
-        data=myfile.read().replace('\n', '').strip()
 
+#   Retrieve page content from passed url string, remove extra tags and return a lxml.html tree object
 def getData(url):
     print "Processing %s" % url
     page = requests.get(url)
-    tree = html.fromstring(page.content.replace('<span>', '').replace('<p>', '').replace('<br>', '').replace("'", ''))
+    
+    #Create tree object from page content with excess tags removed
+    tree = html.fromstring(
+            page.content.replace('<span>', '')
+                .replace('<p>', '')
+                .replace('<br>', '')
+                .replace("'", ''))
     return tree
     
+#   Main functionality method
+#   
 def parseFile(tree, writeFileName, cursor, tableName):
+    #Array containing names of table columns
     colNames = [    'Case Number        ',
                     'Report Date/Time   ',
                     'Occurred Date/Time ',
                     'Incident           ', 
                     'Location           ',
                     'Disposition        '
+    #Extra empty elements in case table row is malformed or parsed incorrectly
     ,'', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']; 
     
+    #Folder names for output files
     outputFolder = 'output_SQL/'
     skippedFolder = 'skipped/'
     
+    #Create output files
     outFile = open(outputFolder + writeFileName, "wb")
     outFileSkipped = open(outputFolder + skippedFolder + writeFileName, "wb")
+    
+    
     recordedRows = 0
     skippedRows = 0
-    retArr = []
-    tbl = tree.xpath('//section[@id=\'maincontent\']')
-    tr = tbl[0].xpath('//tbody/tr')
-    for i in range(1,len(tr)):
-        td = tr[i].xpath('td//text()')
-        if len(td) == 6 and td[0] != '':
-            retStr = "INSERT INTO "+ tableName +" VALUES("
-            for j in range(len(td)-1):
-                g = td[j].strip()
-                h = " ".join(g.split())
-                retStr = retStr + "'" + h + "', "
-                #outStr = "%s: %s" % (colNames[j], h)
-                #print outStr
-                #outFile.write(outStr)
-                #outFile.write("\n")
-            #outFile.write("\n")
+    
+    #Array to hold generated SQL statements
+    sqlStatements = []
+    
+    #HTML parsing code... This might get rough
+    tbl = tree.xpath('//section[@id=\'maincontent\']')  #tbl[] will only have one element, the table with the attribute 'maincontent'
+    tr = tbl[0].xpath('//tbody/tr')                     #Select all <tr> elements within the <tbody> tag       
+    for i in range(1,len(tr)):          
+        td = tr[i].xpath('td//text()')                      #Select the text of each <td> element
+        if len(td) == 6 and td[0] != '':                    #Make sure there are exactly 6 elements in the row and the first isn't empty
+            statement = "INSERT INTO "+ tableName +" VALUES("  #SQL statement
+            for j in range(len(td)-1):                      
+                g = td[j].strip()           #strip leading and trailing whitespace
+                h = " ".join(g.split())     #strip extra whitespace from the middle of string
+                statement = statement + "'" + h + "', " #Add formatted values to statement string
+            
+            #We have to process the last element separately because we need it to end without a comma and with closing parentheses
             g = td[len(td)-1].strip()
             h = " ".join(g.split())
-            retStr = retStr + "'" + h + "')"
-            outFile.write(retStr)
+            statement = statement + "'" + h + "')"
+            
+            #Write statement to file
+            outFile.write(statement)
             outFile.write(";\n\n")
+            
             recordedRows = recordedRows + 1
-            retArr.append(retStr)
+            sqlStatements.append(statement) #add statement to return array
+        
+        #Document malformed row values. We don't add these to the SQL statement array, but we write them to a file so we can see what's wrong
         else:
             outFileSkipped.write(html.tostring(tr[i]))
             for j in range(len(td)):
@@ -192,8 +219,12 @@ def parseFile(tree, writeFileName, cursor, tableName):
             outFileSkipped.write("\n")
             skippedRows = skippedRows + 1
             #print "Malformed row: %d in %s" % (i, writeFileName)
+    #Close file objects
     outFile.close()
-    print "%d/%d rows skipped" % (skippedRows, recordedRows + skippedRows)
-    return retArr
+    outFileSkipped.close()
+    
+    print "%d/%d rows skipped" % (skippedRows, recordedRows + skippedRows)  #print the numbers of successful and malformed rows
+    
+    return sqlStatements    #Return the array of statements
 
 main()
